8ac9e5e40320678900c3847bdc0ca22f90929a9c
Kevin Hoyt <parkerkrhoyt@gmail.com>
2017-09-01 14:33:29 -0600

    Building business model to match.
    Composer business model and transaction files to match application
    structure.

---
+++ Blockchain/composer/lib/logic.js
@@ -0,0 +1,13 @@
+/**
+  * Trade schwag
+  * @param  {org.acme.model.TradeSchwag} tradeSchwag - the trade schwag transaction
+  * @transaction
+  */
+function tradeSchwag( tx ) {
+  tx.item.owner = tx.owner;
+  return getAssetRegistry( 'org.acme.model.Schwag' )
+    .then( function( registry ) {
+      return registry.update( tx.item );
+    } );
+  }
+ 
